8f0e48c0ea0c6855f37adfb1213e691a3ada2c8f
Haseeb702 <80732008+Haseeb702@users.noreply.github.com>
2025-06-22 02:34:24 -0400

    feat/added-profile-pic

--- src/containers/greeting/Greeting.js
+++ src/containers/greeting/Greeting.js
@@ -2,12 +2,11 @@ import React, {useContext} from "react";
 import {Fade} from "react-reveal";
 import emoji from "react-easy-emoji";
 import "./Greeting.scss";
-import landingPerson from "../../assets/lottie/landingPerson";
-import DisplayLottie from "../../components/displayLottie/DisplayLottie";
 import SocialMedia from "../../components/socialMedia/SocialMedia";
 import Button from "../../components/button/Button";
-import {illustration, greeting} from "../../portfolio";
+import {greeting} from "../../portfolio";
 import StyleContext from "../../contexts/StyleContext";
+import profilePic from "../../assets/images/profile.png";
 
 export default function Greeting() {
   const {isDark} = useContext(StyleContext);
@@ -53,14 +52,11 @@ export default function Greeting() {
             </div>
           </div>
           <div className="greeting-image-div">
-            {illustration.animated ? (
-              <DisplayLottie animationData={landingPerson} />
-            ) : (
-              <img
-                alt="man sitting on table"
-                src={require("../../assets/images/manOnTable.svg")}
-              ></img>
-            )}
+            <img
+              src={profilePic}
+              alt="Haseeb Afzal"
+              className="profile-image"
+            />
           </div>
         </div>
       </div>
